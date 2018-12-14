9ff0303b06e209aec89d70210a4d0c303816a2ee
Allen Woods <allen.h.woods@gmail.com>
2018-12-13 21:24:28 -0800

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+# Language constant
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+
+#
+# I will need to research the inner workings of how to do this in much greater
+# detail to fully understand what is happening here.
+#
 def alphabetize(arr)
-  # code here
+  sorted_array = arr.sort_by do |phrase|
+    phrase.chars.map { |alpha_char| ESPERANTO_ALPHABET.index(alpha_char) }
+  end
 end
