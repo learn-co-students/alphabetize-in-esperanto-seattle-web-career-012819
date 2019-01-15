1757a697c9ff4029a2091e3180bce9b370625729
Nat-Reid <nathanielreid@gmail.com>
2019-01-14 16:49:34 -0800

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,18 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
+
 def alphabetize(arr)
   # code here
+  arr.map{|phrase| phrase.split("")}.sort do |p1, p2|
+    equivalence = 0
+    p_index = 0
+
+    while(p_index < [p1.size,p2.size].min && equivalence == 0)
+      p1_char = ESPERANTO_ALPHABET.index(p1[p_index])
+      p2_char = ESPERANTO_ALPHABET.index(p2[p_index])
+
+      equivalence = (p1_char <=> p2_char)
+      p_index += 1
+    end
+    equivalence
+  end.map{|phrase| phrase.join}
 end
