03ef6094f18d62d095dd5516c684e632a27917c0
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-24 22:49:46 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,6 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |x|
     x.split("").map do |char|
       ESPERANTO_ALPHABET.index(char)
