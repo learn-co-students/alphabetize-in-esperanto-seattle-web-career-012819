9161ed82976637b770395a160ba473a0bc5999c1
Madison Stankevich <mnstankevich@crimson.ua.edu>
2019-01-26 16:49:41 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
-  # code here
+  abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdef"
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  
+  arr.sort_by {|character| character.tr(esperanto, abc)}
 end
+
+#".tr" directly translates individual characters in two matching sets. With more complicated sorting, ".gsub" would be preferred, but it is slower than ".tr"
+#can also use ascii codes as a string to translate against. It is unclear to me why "\\" is considered a single symbol in ascii sorting for the 29th char. (ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\")
